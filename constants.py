6ae765a14c953b0807926c0d7f18387671cbba42
Muhammad Azmat <muhammadazmat018@gmail.com>
2026-07-28 10:09:10 +0500

    feat: Project directories generalized

--- constants.py
+++ constants.py
@@ -1,1 +1,9 @@
+import os
+
 RANDOM_SEED = 42
+
+PROJECT_ROOT = "D:/CitrusBits/pythonic-rebirth"
+DATASETS_DIR = os.path.join(PROJECT_ROOT, "datasets")
+MODELS_DIR = os.path.join(PROJECT_ROOT, "models")
+GENERAL_IMAGES_DIR = os.path.join(PROJECT_ROOT, "images")
+OUTPUTS_DIR = os.path.join(PROJECT_ROOT, "outputs")
